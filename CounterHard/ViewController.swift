0bac84cdea5e9ab9e1b0be7be0c679fe3b1f82d8
Igor <aksenov.ivd@gmail.com>
2024-04-23 18:37:44 +0300

    Задача вроде выполнена, я старался

--- CounterHard/ViewController.swift
+++ CounterHard/ViewController.swift
@@ -8,13 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    
+    @IBOutlet weak var textViewHistory: UITextView!
+    
+    @IBOutlet weak var labelTapQuantity: UILabel!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var labelIncrementionButton: UIButton! // Добавляем аутлет для кнопки
+    
+    var totalValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Проект создан")
-        // Do any additional setup after loading the view.
+        textViewHistory.isEditable = false
+       
+        
     }
     
-
+    @IBAction func incrementionOfLabelValue(_ sender: Any) {
+        textViewHistory.text = "История изменений:"
+        totalValue = 0
+        labelTapQuantity.text = "Значение счетчика: \(totalValue) "
+        textViewHistory.text +=
+        "\n\(getCurrentDateTime()): значение сброшено"
+    }
+    
+    @IBAction func buttonPlusAct(_ sender: Any) {
+        totalValue += 1
+        updateLabelAndHistory(value: totalValue, action: "+")
+    }
+    
+    @IBAction func buttonMinusAction(_ sender: Any) {
+        if totalValue > 0 {
+            totalValue -= 1
+            updateLabelAndHistory(value: totalValue, action: "-")
+        } else {
+            textViewHistory.text += "\n\(getCurrentDateTime()): попытка уменьшить значение счётчика ниже 0"
+            labelTapQuantity.text = "ошибка, нажмите '+'"
+        }
+    }
+    
+    func updateLabelAndHistory(value: Int, action: String) {
+        labelTapQuantity.text = "Значение счетчика: \(value)"
+        textViewHistory.text += "\n\(getCurrentDateTime()): значение изменено на \(action)1"
+    }
+    
+    func getCurrentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
 }
-
